8df01cc321a202f22c63a90107925393bf0e755e
Pau Ramos <pauramosimo@gmail.com>
2025-03-09 12:52:02 +0100

    new render

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,126 +6,82 @@
 /*   By: pau <pau@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 20:40:31 by pau               #+#    #+#             */
-/*   Updated: 2025/02/17 15:58:16 by pau              ###   ########.fr       */
+/*   Updated: 2025/03/09 12:49:20 by pau              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/cub3d.h"
+#include "../include/cub3d.h"
 
-int	close_window(t_game	*game)
+void	clean_game(t_data *data)
 {
-	ft_printf("GAME FINISHED\n");
-	free(game);
+	if (data->file_data.file_cpy)
+		free_array(data->file_data.file_cpy);
+	if (data->map)
+		free_array(data->map);
+	if (data->dmap)
+		free_array(data->dmap);
+	free_info(data);
+}
+
+void	game_error(t_data *data, char *error)
+{
+	ft_printf(error);
+	clean_game(data);
+	// destroy_images(data);
+	// if (data->win)
+	// 	mlx_destroy_window(data->mlx, data->win);
+	// if (data->mlx)
+	// {
+	// 	mlx_destroy_display(data->mlx);
+	// 	free(data->mlx);
+	// }
+	exit(1);
+}
+
+static int	init_game(t_data *data)
+{
+	init_cameraplane(data);
+	data->mlx = mlx_init();
+	if (!data->mlx)
+		return (0);
+	data->win = mlx_new_window(data->mlx, WIDTH, HEIGHT, "Cube3D");
+	if (data->win == NULL)
+		return (0);
+	init_img(data);
+	mlx_loop_hook(data->mlx, &render, data);
+	mlx_hook(data->win, 2, 1L << 0, key_event, data);
+	mlx_hook(data->win, 17, 1L << 17, close_window, data);
+	mlx_loop(data->mlx);
+	return (1);
+}
+
+static void	check_validity(t_data *data)
+{
+	duplicate_map(data);
+	floodfill(data->dmap, data->pc.pos.x, data->pc.pos.y);
+	if (!map_validity(data))
+		game_error(data, "Error in map validation\n");
+	else if (!info_validity(data->tex))
+		game_error(data, "Error in textures validation\n");
+	else
+		ft_printf("File content is valid\n");
+}
+
+int	main(int argc, char **argv)
+{
+	t_data	data;
+
+	if (argc != 2)
+	{
+		ft_printf("Error: one argument expected\n");
+		return (1);
+	}
+	init_data(&data);
+	if (!parse(&data, argv))
+		game_error(&data, "Error during parsing\n");
+	init_player(&data);
+	check_validity(&data);
+	if (!init_game(&data))
+		game_error(&data, "Error initializing game\n");
 	return (0);
 }
-
-void	start_values(t_game *game, char **argv)
-{
-	game->img = malloc(sizeof(t_image));
-	game->map = malloc(sizeof(t_map));
-	game->ply = malloc(sizeof(t_ply));
-	game->ray = malloc(sizeof(t_ray));
-
-	game->buffer_data = NULL;
-	game->ply->pdx = 2.0;
-	game->ply->pdy = 0.0;
-	game->ply->pa = 0.0;	
-	game->map->map = ft_read(argv);
-	game->map->width = count_bytes_w_fd(game->map->map[0]);
-	game->map->height = count_lines_w_fd(game->map->map);
-	// game->colors = get_colors(game->map->map);
-}
-
-int main(int argc, char **argv)
-{
-	t_game game;
-	
-	if(argc > 2)
-		return(0);
-	start_values(&game, argv);
-	game.img->mlx = mlx_init();
-	game.img->mlx_win = mlx_new_window(game.img->mlx, WIDTH,
- 			HEIGHT, "Cub3d");
-
-	// draw_map(&game);
-	// mlx_loop_hook(game.img->mlx, &render, &game);
-	mlx_loop_hook(game.img->mlx, render, &game);
-	mlx_hook(game.img->mlx_win, 17, 0, close_window, game.img);
-	mlx_hook(game.img->mlx_win, 2, 1L << 0, key_press, &game);
-	mlx_loop(game.img->mlx);
-}
-// int	main(int argc, char **argv)
-// {
-// 	t_image	img;
-
-// 	if(argc == 0)
-// 		return(0);
-// 	img.map = ft_read(argv);
-// 	start_values(&img);
-// 	img.x_player = (count_bytes_w_fd(img.map[0]) * 64) / 2;
-// 	img.y_player = (count_lines_w_fd(img.map) * 64) / 2;
-// 	img.mlx = mlx_init();
-// 	printf("%i\n", count_lines_w_fd(img.map) * 64);
-// 	img.mlx_win = mlx_new_window(img.mlx, count_bytes_w_fd(img.map[0]) * 64 * 2,
-// 			count_lines_w_fd(img.map) * 64, "cub3d");
-// 	draw_map(&img);
-// 	img.y_pixel = img.y_player;
-//     img.x_pixel = img.x_player;
-// 	mlx_hook(img.mlx_win, 17, 0, close_window, &img);
-// 	mlx_hook(img.mlx_win, 2, 1L << 0, key_press, &img);
-// 	mlx_loop(img.mlx);
-// }
-
-
-
-
-
-
-
-// int	key_press(int key_code, t_image *img)
-// {
-// 	if (key_code == 100)
-// 		right(img);
-// 	if (key_code == 97)
-// 		left(img);
-// 	move_check(img);
-// 	if (key_code == 119)
-// 		up(img);
-// 	if (key_code == 115)
-// 		down(img);
-// 	if (key_code == 65307)
-// 	{
-// 		ft_printf("GAME FINISHED\n");
-// 		free_list(img);
-// 	}
-// }
-
-
-
-
-
-
-
-
-
-// void		start_values(t_image *img)
-// {
-// 	img->player = (t_player *)malloc(sizeof(t_player));
-// 	if (img->player == NULL)
-// 		return (free_list(img));
-// 	img->ray = (t_raycasting *)malloc(sizeof(t_raycasting));
-// 	if (img->ray == NULL)
-// 		return (free_list(img));
-// 	img->check = (t_move_check *)malloc(sizeof(t_move_check));
-// 	if (img->check == NULL)
-// 		return (free_list(img));
-// 	img->player->px = img->x_player;
-// 	img->player->py = img->y_player;
-// 	img->player->pdx = 5.0;
-// 	img->player->pdy = 0.0;
-// 	img->player->pa = 0.0;
-
-// 	img->win_width = count_bytes_w_fd(img->map[0]);
-// 	img->win_height = count_lines_w_fd(img->map);
-// }
-
--- src/render.c
+++ src/render.c
@@ -6,66 +6,49 @@
 /*   By: pau <pau@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 20:57:43 by pau               #+#    #+#             */
-/*   Updated: 2025/02/17 16:30:00 by pau              ###   ########.fr       */
+/*   Updated: 2025/03/09 12:50:47 by pau              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/cub3d.h"
-int render(t_game *game) {
-    void *img_buffer;
-    int bits_per_pixel;
-    int size_line;
-    int endian;
+#include "../include/cub3d.h"
 
-    // Crear un nuevo buffer de imagen
-    img_buffer = mlx_new_image(game->img->mlx, WIDTH, HEIGHT);
-    if (!img_buffer) {
-        fprintf(stderr, "Error: Failed to create image buffer\n");
-        return -1; // Manejo de error
-    }
-    
-    // Obtener la dirección del buffer de datos
-    game->buffer_data = mlx_get_data_addr(img_buffer, &bits_per_pixel, &size_line, &endian);
+void	put_pixel(t_data *data, int x, int y, int color)
+{
+	char	*pixel;
 
-    // Limpiar el buffer
-    ft_memset(game->buffer_data, 0, WIDTH * HEIGHT * (bits_per_pixel / 8));
-
-    // Dibujar el mapa en el buffer
-    draw_map(game);
-    draw_pixel(game, (int)game->map->player_x, (int)game->map->player_y, 0x00FF0000); // Dibuja al jugador
-    draw_line_until_wall(game, 0x00FF00); // Dibuja la línea hasta la pared
-    
-    // Poner el buffer en la ventana
-    mlx_put_image_to_window(game->img->mlx, game->img->mlx_win, img_buffer, 0, 0);
-    
-    // Destruir el buffer después de usarlo
-    mlx_destroy_image(game->img->mlx, img_buffer);
-
-    return (0);
+	if (y < 0 || y > HEIGHT - 1 || x < 0
+		|| x > WIDTH - 1)
+		return ;
+	pixel = (data->img[4].ptr + (y * data->img[4].stride
+				+ x * (data->img[4].bpp / 8)));
+	*(int *)pixel = color;
 }
 
+static void	render_background(t_data *data)
+{
+	int	x;
+	int	y;
 
-// // void draw_sky_and_floor(t_game *game)
-// // {
-// //     int x;
-// //     int y;
+	x = 0;
+	while (x < WIDTH)
+	{
+		y = 0;
+		while (y < HEIGHT)
+		{
+			if (y < HEIGHT / 2)
+				put_pixel(data, x, y, number_to_hex(data->tex.c));
+			else
+				put_pixel(data, x, y, number_to_hex(data->tex.f));
+			y++;
+		}
+		++x;
+	}
+}
 
-// //     x = 0;
-// // 	while (x < WIDTH)
-// // 	{
-// // 		y = 0;
-// // 		while (y < HEIGHT)
-// // 		{
-// // 			if (y < HEIGHT / 2)
-// // 				mlx_pixel_put(game->img->mlx, game->img->mlx_win, x, y, 
-// // 				ft_atoi(rgb_to_hex(game->colors->ceiling_r, game->colors->ceiling_g,
-// // 				game->colors->ceiling_b)));
-// // 			else
-// // 				mlx_pixel_put(game->img->mlx, game->img->mlx_win, x, y,
-// // 				ft_atoi(rgb_to_hex(game->colors->floor_r, game->colors->floor_g,
-// // 				game->colors->floor_b)));
-// // 			y++;
-// // 		}
-// // 		++x;
-// // 	}
-// // }
+int	render(t_data *data)
+{
+	render_background(data);
+	raycasting(data);
+	mlx_put_image_to_window(data->mlx, data->win, data->img[4].mlx_img, 0, 0);
+	return (0);
+}
